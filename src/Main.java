b07481c03427b8ab36989277c586c40c176db45c
jvilla1991 <villajeremy1991@gmail.com>
2017-06-06 10:51:41 -0400

    added comments

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
+// Calculate the batting statistics of a person at bat
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         double average = 0.000;
         double slug = 0.000;
-        double total = 0.000;
+        double totalBatResult = 0.000;
         double batHit = 0.000;
         boolean bool1 = false;
         boolean bool2 = true;
@@ -17,10 +19,11 @@ public class Main {
             System.out.println("Please enter how many at bats for player");
             inputSize = scnr.nextInt();
 
+            // The user needs to define the number of elements for the array which they will be storing information in
             double[] atBatArray = new double[inputSize];
 
 
-
+            // Loops through and populates each element with user input, must be 1-4
             for (int i = 0; i < atBatArray.length; i++) {
                 do{
                     System.out.println("Enter at bat result for player at current bat(0-4): " + (i + 1));
@@ -37,7 +40,7 @@ public class Main {
                 } while(bool1 == true);
 
 
-                total = atBatArray[i] + total;
+                totalBatResult = atBatArray[i] + totalBatResult;
 
                 if (atBatArray[i] > 0)
                 {
@@ -45,24 +48,36 @@ public class Main {
                 }
             }
 
+            // Both System Outs call methods that calculate the Batting Average and the Slugging Average respectively.
             System.out.println("Batting Average: " + batAverage(batHit, inputSize));
-            System.out.println("Slugging Average of player: " + slugAverage(total, inputSize));
+            System.out.println("Slugging Average of player: " + slugAverage(totalBatResult, inputSize));
+
+            totalBatResult = 0;
+            batHit = 0;
 
 
             scnr.nextLine();
 
-
             System.out.println("Would you like to know more about another player? (y/n)?");
             String s = scnr.nextLine();
-            if ("y".equalsIgnoreCase(s))
-            {
-                bool2 = true;
-            }
-            else if ("n".equalsIgnoreCase(s))
-            {
-                bool2 = false;
-            }
+
+            // Once again, experimenting with rerun prompts from the last lab. This one is store in a method.
+            bool2 = promptRerun(s);
         } while (bool2 == true);
+    }
+    
+    private static boolean promptRerun(String s)
+    {
+        boolean bool2 = true;
+        if ("y".equalsIgnoreCase(s))
+        {
+             bool2 = true;
+        }
+        else if ("n".equalsIgnoreCase(s))
+        {
+             bool2 = false;
+        }
+        return bool2;
     }
 
     private static double slugAverage(double total, int inputSize)
